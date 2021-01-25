a407be66732869f48f4fab3a8d72020c594db809
Salimyaro <3508880@i.ua>
2021-01-25 18:42:55 +0200

    TypeScript refactoring

---
+++ README.md
@@ -0,0 +1,2 @@
+компиляция ts в js
+https://metanit.com/web/typescript/1.2.php
--- js/task-3.ts
+++ js/task-3.ts
@@ -1,20 +1,21 @@
 // Напиши скрипт имитирующий авторизацию администратора в панели управления.
+
 // Есть переменная message в которую будет записано сообщение о результате.При загрузке страницы у посетителя запрашивается пароль через prompt:
+
 // Если нажали Cancel, записать в message строку 'Отменено пользователем!'
 // В протовном случае, если введен пароль который совпадает со значением константы ADMIN_PASSWORD, записать в message строку 'Добро пожаловать!'
 // В противном случае, то есть если ни одно из предыдущих условий не выполнилось, записать в message строку 'Доступ запрещен, неверный пароль!'
 // После всех проверок вывести в alert значение переменной message.
 'use strict';
-var ADMIN_PASSWORD = 'jqueryismyjam';
-var message;
-var input1 = prompt('Enter password:');
+
+const ADMIN_PASSWORD: string = 'jqueryismyjam';
+let message: string;
+let input1: null | string = prompt('Enter password:');
 if (input === null) {
-    message = 'Отменено пользователем!';
-}
-else if (input1 === ADMIN_PASSWORD) {
-    message = 'Добро пожаловать!';
-}
-else {
-    message = 'Доступ запрещен, неверный пароль!';
+  message = 'Отменено пользователем!';
+} else if (input1 === ADMIN_PASSWORD) {
+  message = 'Добро пожаловать!';
+} else {
+  message = 'Доступ запрещен, неверный пароль!';
 }
 alert(message);
